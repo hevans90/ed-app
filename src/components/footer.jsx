70f51aebc2c373d61b0127423fd1b9af878e2c92
eddburdett <544burdett@gmail.com>
2022-02-01 20:11:34 +0000

    coding adventures

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 
 export const Footer = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(2);
 
   const reaction = useRef('initial');
 
   useEffect(() => {
     reaction.current = count < 5 ? 'you suck' : 'awesome';
   }, [count]);
+
+  if(count === 12) {
+    return <div>You FAILED</div>
+  }
 
   return (
     <div>
